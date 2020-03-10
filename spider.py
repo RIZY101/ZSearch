15c4dda3024b460a2fdff2bfe7905745216b950d
Richie Zins <rizins@pop-os.localdomain>
2020-03-09 23:18:27 -0700

    I started my web spider for find links to other pages within pages. So far it will only work for websites that have links embedded in their html, but not for sites where the links are generated with javascript.

---
+++ spider.py
@@ -0,0 +1,30 @@
+# Author: Richard Zins
+
+from bs4 import BeautifulSoup
+import requests
+import queue 
+
+url = input("Enter the starting website URl: ")
+q = queue.Queue()
+q.put(url)
+
+while not q.empty() :
+
+    entry = q.get()
+    request = requests.get(entry)
+    data = request.text
+    soup = BeautifulSoup(data, "html.parser")
+    #  print(soup.prettify())
+
+    for link in soup.find_all("a") :
+        str = link.get("href")
+
+        if "http" in str :
+             q.put(str)
+             print(str)
+        else :
+             locationOfDotCom = entry.find(".com")
+             newEntry = entry[0 : locationOfDotCom + 4]
+             str2 = newEntry + str
+             q.put(str2)
+             print(str2)
